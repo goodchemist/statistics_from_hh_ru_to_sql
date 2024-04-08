97ea5e49fc5a52098f9e8df6a36ec0531bb9daf3
good_chemist <iolgasolovyeva@gmail.com>
2024-04-08 17:03:01 +0700

    add get_employer_info()

--- src/headhunterapi.py
+++ src/headhunterapi.py
@@ -1,5 +1,9 @@
+import requests
+
+
 class HeadHunterAPI:
     """Класс для работы с API HeadHunter."""
+
     def __init__(self, employer_id: int):
         """
         Создание экземпляра класса HeadHunterAPI, где в employer_info будет храниться информация о работодателе,
@@ -12,3 +16,17 @@ class HeadHunterAPI:
         self.employer_id = employer_id
         self.employer_info = {}
         self.vacancies = []
+
+    def get_employer_info(self) -> None:
+        """Получает информацию о работодателе."""
+
+        url = f'https://api.hh.ru/employers/{self.employer_id}'
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Вызов исключения для кодов статуса 4xx/5xx
+            data_json = response.json()
+            self.employer_info = {'company_name': data_json['name'], 'url': data_json['site_url'],
+                                  'vacancies': data_json['open_vacancies']}
+
+        except requests.exceptions.RequestException as e:
+            raise Exception(f'Ошибка при выполнении запроса: {e}.')
