acc08139a21926fd7391aa044e333103f40b9bdc
good_chemist <iolgasolovyeva@gmail.com>
2024-04-08 17:03:52 +0700

    add get_vacancies()

--- src/headhunterapi.py
+++ src/headhunterapi.py
@@ -30,3 +30,61 @@ class HeadHunterAPI:
 
         except requests.exceptions.RequestException as e:
             raise Exception(f'Ошибка при выполнении запроса: {e}.')
+
+    def get_vacancies(self) -> None:
+        """Получает информацию о вакансиях работодателя."""
+
+        url = f'https://api.hh.ru/vacancies?employer_id={self.employer_id}'
+
+        try:
+            response = requests.get(url, params={})
+
+            response.raise_for_status()  # Вызов исключения для кодов статуса 4xx/5xx
+
+            data_json = response.json()
+
+            for vacancy_data in data_json['items']:
+                name = vacancy_data['name']
+
+                try:
+                    salary_from = vacancy_data['salary']['from']
+                except TypeError:
+                    salary_from = '--'
+
+                if salary_from is None:
+                    salary_from = '--'
+
+                try:
+                    salary_to = vacancy_data['salary']['to']
+                except TypeError:
+                    salary_to = None
+
+                if salary_to is None:
+                    salary_to = '--'
+
+                try:
+                    salary_currency = vacancy_data['salary']['currency']
+                except TypeError:
+                    salary_currency = 'Валюта не указана'
+
+                if salary_currency is None:
+                    salary_currency = 'Валюта не указана'
+
+                city = vacancy_data['area']['name']
+                description = vacancy_data['snippet']['responsibility']
+                url_vacancy = vacancy_data['alternate_url']
+
+                vacancy = {
+                    'name': name,
+                    'salary_from': salary_from,
+                    'salary_to': salary_to,
+                    'salary_currency': salary_currency,
+                    'city': city,
+                    'description': description,
+                    'url_vacancy': url_vacancy
+                }
+
+                self.vacancies.append(vacancy)
+
+        except requests.exceptions.RequestException as e:
+            raise Exception(f'Ошибка при выполнении запроса: {e}.')
