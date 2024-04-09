220c81f37d19864fb43fb3d2d531d1115750da05
good_chemist <iolgasolovyeva@gmail.com>
2024-04-09 16:46:57 +0700

    create get_all_vacancies()

--- src/db_manager.py
+++ src/db_manager.py
@@ -39,3 +39,31 @@ class DBManager:
         finally:
             if conn is not None:
                 conn.close()
+
+    def get_all_vacancies(self) -> list:
+        """Получает список всех вакансий с указанием названия компании,
+        названия вакансии, зарплаты и ссылки на вакансию.
+        :return: list
+        """
+        try:
+            with psycopg2.connect(**self.params) as conn:
+                with conn.cursor() as cur:
+                    data = []
+                    cur.execute(f"""SELECT name, {self.tb_vacancies}.url, {self.tb_employers}.company_name,
+                    salary_to, salary_from, salary_currency
+                    FROM {self.tb_vacancies}
+                    JOIN {self.tb_employers} USING(employer_id)
+                    
+                    """)
+
+                    for company in cur:
+                        data.append(company)
+
+                    return data
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+        finally:
+            if conn is not None:
+                conn.close()
